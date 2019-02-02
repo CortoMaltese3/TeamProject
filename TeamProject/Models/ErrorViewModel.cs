4e83e099d67e375720a7c02eaa838de9d31be6f0
George Xiros <geo.xiros@gmail.com>
2019-02-02 08:47:44 +0200

    Core MVC

---
+++ TeamProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeamProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
