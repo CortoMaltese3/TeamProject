da26f434f6042e07f2bf64fe3d65b3873d692126
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2019-02-06 18:18:33 +0200

    fixed code

--- TeamProject/Controllers/LocationsController.cs
+++ TeamProject/Controllers/LocationsController.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using TeamProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TeamProject
 {
-    public class Location
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public string Description { get; set; }
-    }
+    
     [Route("api/[controller]")]
     [ApiController]
     public class LocationsController : ControllerBase
@@ -21,7 +14,6 @@ namespace TeamProject
         [HttpGet]
         public IEnumerable<Location> Get()
         {
-
             return new List<Location>() {
                 new Location() { Latitude = 37.9838096, Longitude = 23.727538800000048, Description = "Αθηνα" },
                 new Location() { Latitude = 37.990849, Longitude = 23.738339, Description = "Βυρωνας" },
---
+++ TeamProject/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamProject.Models
+{
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Description { get; set; }
+    }
+}
