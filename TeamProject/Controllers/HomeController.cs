c9bf2140944652cea9b7a38593ba058a66f23d32
George Xiros <geo.xiros@gmail.com>
2019-02-06 07:38:05 +0200

    remove using kati

--- TeamProject/Controllers/HomeController.cs
+++ TeamProject/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TeamProject.Models;
 
