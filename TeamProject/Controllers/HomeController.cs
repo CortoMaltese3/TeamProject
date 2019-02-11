d2fc168e594df7b94c223640147cabda4f01530e
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2019-02-11 19:18:24 +0200

    Added connection with Database and changed abit the foreach in home/index

--- TeamProject/Controllers/HomeController.cs
+++ TeamProject/Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamProject.Models;
 
 namespace TeamProject.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+
+            return View(db.Court.ToList());
         }
 
         public ActionResult About()
