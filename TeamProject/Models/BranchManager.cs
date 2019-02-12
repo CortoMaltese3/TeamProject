ffef0aaadae399324057feea4efe12743ae27a5d
geo.xiros <geo.xiros@gmailcom>
2019-02-12 15:45:41 +0200

    frynis Task

---
+++ TeamProject/Models/BranchManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TeamProject.Models
+{
+    public class BranchManager
+    {
+        private ApplicationDbContext _db;
+
+        public BranchManager()//ApplicationDbContext db)
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        /// <summary>
+        /// Returns List of branches near to a given latitude, longtitude
+        /// </summary>
+        /// <param name="latitude"></param>
+        /// <param name="longtitude"></param>
+        /// <returns></returns>
+        public List<Branch> GetNearestBranches(double latitude, double longtitude, double distanceInMeters = 5000)
+        {
+            // TODO
+            // Call GetBranchesDistance Procedure from Sql Server and Return List With branches
+            // Procedure Parameters:
+            //   @Latitude float,
+            //   @Longtitude float,
+            //   @distance float
+            // Returned Columns => ID, Distance
+            throw new NotImplementedException();
+        }
+    }
+}
