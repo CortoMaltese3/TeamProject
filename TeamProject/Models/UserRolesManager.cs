a220a16cf0072ba4c3e10995089210884557e301
George Chaniotakis <haniotinho@gmail.com>
2019-02-20 20:21:32 +0200

    User Roles Manager

---
+++ TeamProject/Models/UserRolesManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace TeamProject.Models
+{
+    public class UserRolesManager : TableManager<UserRoles>
+    {
+        public UserRolesManager(ProjectDbContext projectDbContext)
+        {
+            _queryParts = new Dictionary<string, string>()
+            {
+                { "FindById", "UserRoles.UserId = @id" },
+                { "InsertQuery",
+                    "INSERT INTO UserRoles ([UserId],[Role]) " +
+                    "VALUES (@UserId,@Role)" +
+                    "SELECT * FROM UserRoles WHERE UserRoles.UserId = @id)"},
+                { "RemoveQuery",
+                    "DELETE FROM UserRoles WHERE UserId = @Id" },
+                { "UpdateQuery",
+                    "UPDATE UserRoles SET " +
+                    "[UserId]=@UserId[Role]=@Role, [ImageCourt]=@ImageCourt, [MaxPlayers]=@MaxPlayers, [Price]=@Price " +
+                    "WHERE Id = @Id"}
+            };
+            _db = projectDbContext;
+        }
+
+        public override IEnumerable<UserRoles> Get(string queryWhere = null, object parameters = null)
+        {
+            IEnumerable<UserRoles> userroles = null;
+
+            _db.UsingConnection((dbCon) =>
+            {
+                userroles = dbCon.Query<UserRoles, User, UserRoles>(
+                    "SELECT * FROM Useroles INNER JOIN User ON User.UserId = Branch.Id" + (queryWhere == null ? string.Empty : $" WHERE {queryWhere}"),
+                    (userrole, user) =>
+                    {
+                        userrole.UserId = user.Id;
+                        return userrole;
+                    },
+                    splitOn: "id",
+                    param: parameters)
+                    .Distinct();
+            });
+
+            return userroles;
+        }
+    }
+}
