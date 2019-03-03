3de6c20a9dafa3aba4eb54ccd31c916058942429
George Chaniotakis <haniotinho@gmail.com>
2019-03-03 21:56:09 +0200

    Role model

---
+++ TeamProject/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+public partial class Role
+{
+    public Role()
+    {
+
+    }
+
+    public int Id { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+}
