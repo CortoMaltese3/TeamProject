dcf16934c28eb0ab102d428ab3e6c68f844b899a
George Xiros <geo.xiros@gmail.com>
2019-03-12 23:38:18 +0200

    move timeslots to admin panel

--- TeamProject/Areas/Admin/Controllers/TimeSlotsController.cs
+++ TeamProject/Areas/Admin/Controllers/TimeSlotsController.cs
@@ -8,7 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using TeamProject.Models;
 
-namespace TeamProject.Controllers
+namespace TeamProject.Areas.Admin.Controllers
 {
     public class TimeSlotsController : Controller
     {
