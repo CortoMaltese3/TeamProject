c1152e3694bcc5d913a4cb8f5aebe7358fa1c4fa
George Xiros <geo.xiros@gmail.com>
2019-03-13 23:52:12 +0200

    set datefirst for monday

--- TeamProject/ApiControllers/BookController.cs
+++ TeamProject/ApiControllers/BookController.cs
@@ -14,14 +14,14 @@ namespace TeamProject.ApiControllers
         {
             // TODO 
             // GET TIMESLOTS FOR
+            // SET DATEFIRST 1;
             // SELECT *
             //  FROM[TimeSlot] LEFT JOIN Booking
             // ON TimeSlot.CourtId = Booking.CourtId
-            //AND[TimeSlot].[Day] = DATEPART(WEEKDAY,[Booking].[BookedAt])
-            //AND[TimeSlot].[Hour] = cast([Booking].[BookedAt] as time)
-
-            //WHERE TimeSlot.CourtId = 1
-            //AND([Booking].[BookedAt] BETWEEN '2019-03-11' AND '2019-03-17 23:59:59' OR Booking.Id is null)
+            // AND[TimeSlot].[Day] = DATEPART(WEEKDAY,[Booking].[BookedAt])
+            // AND[TimeSlot].[Hour] = cast([Booking].[BookedAt] as time)
+            // WHERE TimeSlot.CourtId = 1
+            // AND([Booking].[BookedAt] BETWEEN '2019-03-11' AND '2019-03-17 23:59:59' OR Booking.Id is null)
 
             return new string[] { "value1", "value2" };
         }
