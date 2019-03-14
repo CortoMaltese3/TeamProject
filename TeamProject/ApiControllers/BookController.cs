e9a00613ab9105bd687e0f73840c4e1feea46424
geo.xiros <geo.xiros@gmailcom>
2019-03-14 15:20:22 +0200

    Add method to get timeslot for court + ajax call to api

--- TeamProject/ApiControllers/BookController.cs
+++ TeamProject/ApiControllers/BookController.cs
@@ -4,39 +4,39 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TeamProject.Models;
+using TeamProject.ModelsViews;
 
 namespace TeamProject.ApiControllers
 {
     public class BookController : ApiController
     {
+        private ProjectDbContext db = new ProjectDbContext();
         // GET: api/Book
-        public IEnumerable<string> Get()
-        {
-            // TODO 
-            // EXEC GetTimeslotsAt 1,'2019-03-12','2019-03-17 23:59:59'
-
-            return new string[] { "value1", "value2" };
-        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
         // GET: api/Book/5
-        public string Get(int id)
+        public IEnumerable<TimeslotApiView> Get(int id)
         {
-            return "value";
+            return db.TimeSlots.GetForBooking(id);
         }
 
-        // POST: api/Book
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST: api/Book
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT: api/Book/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT: api/Book/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE: api/Book/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE: api/Book/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
--- TeamProject/ModelsViews/TimeslotApiView.cs
+++ TeamProject/ModelsViews/TimeslotApiView.cs
@@ -7,15 +7,13 @@ namespace TeamProject.ModelsViews
 {
     public class TimeslotApiView
     {
-        public int Id { get; set; }
-
-        public int CourtId { get; set; }
-
         public int Day { get; set; }
 
         public TimeSpan Hour { get; set; }
 
-        public int Duration { get; set; }
+        public int Id { get; set; }
+
+        public int CourtId { get; set; }
 
         public int BookingId{ get; set; }
 
