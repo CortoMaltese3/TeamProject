6e80f6fe3303ed8ce69e7c08a16455dd0ac45ebe
George Xiros <geo.xiros@gmail.com>
2019-03-14 07:18:17 +0200

    Create TimeShlotApiView

---
+++ TeamProject/ModelsViews/TimeslotApiView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamProject.ModelsViews
+{
+    public class TimeslotApiView
+    {
+        public int Id { get; set; }
+
+        public int CourtId { get; set; }
+
+        public int Day { get; set; }
+
+        public TimeSpan Hour { get; set; }
+
+        public int Duration { get; set; }
+
+        public int BookingId{ get; set; }
+
+    }
+}
