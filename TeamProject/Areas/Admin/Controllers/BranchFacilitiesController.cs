eaea39739c172075c7f5719112f0f49bad3fe60e
George Xiros <geo.xiros@gmail.com>
2019-03-22 17:50:01 +0200

    Add BranchFacilitiesController and move Addfacilities there

---
+++ TeamProject/Areas/Admin/Controllers/BranchFacilitiesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamProject.Areas.Admin.Controllers
+{
+    public class BranchFacilitiesController
+    {
+        private ProjectDbContext db = new ProjectDbContext();
+        [Authorize(Roles = "Owner,Admin")]
+        public ActionResult ChooseFacilities(int id)
+        {
+            var facilityFormModel = new FacilityFormModel()
+            {
+                AvailableFacilities = db.BranchFacilities.GetFacilities(id),
+                BranchId = id
+            };
+
+            return View(facilityFormModel);
+        }
+
+        public ActionResult AddFacilities(FacilityFormModel facilityFormModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                facilityFormModel.AvailableFacilities = db.BranchFacilities.GetFacilities(facilityFormModel.BranchId);
+                return View(facilityFormModel);
+            }
+
+            // remove all branch facilities
+            db.BranchFacilities.Remove(facilityFormModel.BranchId);
+
+            // add selected facilities
+            var facilities = facilityFormModel.SelectedFacilities;
+            foreach (var item in facilities)
+            {
+                db.BranchFacilities.Add(new BranchFacilities() { BranchId = facilityFormModel.BranchId, FacilityId = item });
+            }
+
+            return RedirectToAction("Index", "Branches");
+        }
+
+    }
+}
--- TeamProject/Areas/Admin/Controllers/FacilitiesController.cs
+++ TeamProject/Areas/Admin/Controllers/FacilitiesController.cs
@@ -42,38 +42,6 @@ namespace TeamProject.Areas.Admin.Controllers
             return View(facility);
         }
 
-        [Authorize(Roles = "Owner,Admin")]
-        public ActionResult ChooseFacilities(int id)
-        {
-            var facilityFormModel = new FacilityFormModel()
-            {
-                AvailableFacilities = db.BranchFacilities.GetFacilities(id),
-                BranchId = id
-            };
-
-            return View(facilityFormModel);
-        }
-
-        public ActionResult AddFacilities(FacilityFormModel facilityFormModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                facilityFormModel.AvailableFacilities = db.BranchFacilities.GetFacilities(facilityFormModel.BranchId);
-                return View(facilityFormModel);
-            }
-
-            // remove all branch facilities
-            db.BranchFacilities.Remove(facilityFormModel.BranchId);
-            
-            // add selected facilities
-            var facilities = facilityFormModel.SelectedFacilities;
-            foreach (var item in facilities)
-            {
-                db.BranchFacilities.Add(new BranchFacilities() { BranchId = facilityFormModel.BranchId, FacilityId = item });
-            }
-
-            return RedirectToAction("Index", "Branches");
-        }
 
         // GET: Facilities/Create
         [Authorize(Roles = "Admin")]
