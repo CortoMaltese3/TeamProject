53aaba74efa76a22d00e15ff55196e0cdf6b52b5
George Xiros <geo.xiros@gmail.com>
2019-03-22 17:46:47 +0200

    Add form model for BranchFacilities

---
+++ TeamProject/Models/FacilityFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamProject.Models
+{
+    public class FacilityFormModel
+    {
+        public int BranchId { get; set; }
+        public IList<int> SelectedFacilities { get; set; }
+        public IList<SelectListItem> AvailableFacilities { get; set; }
+
+        public FacilityFormModel()
+        {
+            SelectedFacilities = new List<int>();
+            AvailableFacilities = new List<SelectListItem>();
+        }
+    }
+}
