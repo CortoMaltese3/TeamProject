a5c93d4a099c51c5e14fe908b2e0a2cc761c6be0
George Xiros <geo.xiros@gmail.com>
2019-03-24 00:03:01 +0200

    add bookings popover info

---
+++ TeamProject/ApiControllers/BookingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TeamProject.Models;
+using TeamProject.ModelsViews;
+
+
+namespace TeamProject.ApiControllers
+{
+    [Authorize(Roles = "Admin,Owner")]
+    public class BookingsController : ApiController
+    {
+        private ProjectDbContext db = new ProjectDbContext();
+        // GET: api/Bookings/5
+        public User Get(int id)
+        {
+            return db.Bookings
+                .Get("Booking.Id=@id", new { id })
+                .FirstOrDefault()
+                .User;
+        }
+    }
+}
