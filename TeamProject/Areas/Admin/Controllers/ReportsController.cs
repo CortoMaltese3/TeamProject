08d99ca93c73762376e871e0dadd553afa6bdd7c
George Chaniotakis <haniotinho@gmail.com>
2019-03-26 21:56:06 +0200

    Reports initialized

---
+++ TeamProject/Areas/Admin/Controllers/ReportsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamProject.Models;
+
+namespace TeamProject.Areas.Admin.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private ProjectDbContext db = new ProjectDbContext();
+
+        // GET: Reports
+        [Authorize (Roles = "Admin, Owner")]
+        public ActionResult Index(int id)
+        {
+
+            var courts = db.Courts.Get().Where(c => c.BranchId == id);
+            ViewBag.Data = "data: [300, 50, 100, 40, 120]";
+            return View(courts);
+        }
+    }
+}
