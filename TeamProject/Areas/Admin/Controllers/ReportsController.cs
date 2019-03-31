283a041901f308d7260452718dbfa3f5c07b92f6
George Chaniotakis <haniotinho@gmail.com>
2019-03-31 12:03:38 +0300

    attempting to generate reports for courts

--- TeamProject/Areas/Admin/Controllers/ReportsController.cs
+++ TeamProject/Areas/Admin/Controllers/ReportsController.cs
@@ -16,12 +16,30 @@ namespace TeamProject.Areas.Admin.Controllers
         [Authorize(Roles = "Admin, Owner")]
         public ActionResult Index(int id)
         {
-            var branchreport = db.Branches.GetBookingsByBranchAndDay(id);
-            //var courtsreport = db.Branches.GetBookingsByCourtAndDay(id);
+            var branchreport = db.Branches.GetBookingsByBranchAndDay(id);            
             var courts = db.Courts.Get().Where(c => c.BranchId == id);
             ViewBag.Data = string.Join(",", branchreport.OrderBy(b => b.BookingDayNo).Select(b => b.CountOfBookings)) ;
             ViewBag.Labels = string.Join(",", branchreport.OrderBy(b => b.BookingDayNo).Select(b => "\"" + b.BookingDay + "\""));
+
+            //var courtsdict = new Dictionary<int, BookingReport>();
+            //foreach(var c in courts)
+            //{
+            //    var courtsreport = db.Branches.GetBookingsByCourtAndDay(c.Id);
+
+            //    courtsdict.Add(c.Id, courtsreport);
+            //}
+            var report = new ReportsView()
+            {
+                BranchesReport = branchreport
+            };
+
             return View(courts);
         }
+
+    }
+    public class ReportsView
+    {
+        public IEnumerable<BookingReport> BranchesReport { get; set; }
+
     }
 }
