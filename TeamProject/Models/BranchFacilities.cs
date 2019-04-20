1f52f57bf66ac5687719ee4aa7fbb809dc9bfe84
George Xiros <geo.xiros@gmail.com>
2019-04-20 10:08:57 +0300

    remove dead code in comments

--- TeamProject/Models/BranchFacilities.cs
+++ TeamProject/Models/BranchFacilities.cs
@@ -7,7 +7,7 @@ using TeamProject.Dal;
 
 namespace TeamProject.Models
 {
-    public class BranchFacilities : IEquatable<BranchFacilities>
+    public class BranchFacilities
     {
         [TableField(PrimaryKey = true)]
         public int BranchId { get; set; }
@@ -18,18 +18,6 @@ namespace TeamProject.Models
         public Branch Branch { get; set; }
         public Facility Facility { get; set; }
 
-        // used for union with available facilitie in BranchFacilities Controller.ChooseFacilities Method
-        public bool Equals(BranchFacilities other)
-        {
-            if (other == null)
-            {
-                return false;
-            }
-
-            return FacilityId == other.FacilityId;
-        }
-        public override bool Equals(object obj) => Equals(obj as BranchFacilities);
-        public override int GetHashCode() => FacilityId.GetHashCode();
     }
 
 }
