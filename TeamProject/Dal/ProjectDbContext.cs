9ce8c7fe70e36c36049ea66a219bd8af074d2b1e
George Xiros <geo.xiros@gmail.com>
2019-04-21 23:31:08 +0300

    ProjectDbContext TODOs

--- TeamProject/Dal/ProjectDbContext.cs
+++ TeamProject/Dal/ProjectDbContext.cs
@@ -17,17 +17,21 @@ namespace TeamProject.Dal
         public ProjectDbContext()
         {
             _connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            Courts = new CourtManager(this);
+
+            // TODO : initialize entities managers using Dependency Injection
+            Bookings = new BookingManager(this);
             Branches = new BranchManager(this);
             BranchFacilities = new BranchFacilitiesManager(this);
-            Users = new UserManager(this);
+            Courts = new CourtManager(this);
             Facilities = new FacilityManager(this);
-            UserRoles = new UserRolesManager(this);
-            TimeSlots = new TimeSlotManager(this);
             Reviews = new ReviewManager(this);
-            Bookings = new BookingManager(this);
             Roles = new RoleManager(this);
+            TimeSlots = new TimeSlotManager(this);
+            Users = new UserManager(this);
+            UserRoles = new UserRolesManager(this);
         }
+
+        // TODO : use OperationResult to get error message
         public void UsingConnection(Action<SqlConnection> action)
         {
             LastActionError = string.Empty;
@@ -43,15 +47,17 @@ namespace TeamProject.Dal
                 LastActionError = e.Message;
             }
         }
-        public IDatabaseActions<Court> Courts { get; set; }  
+
+        public IDatabaseActions<Booking> Bookings { get; set; }
         public IDatabaseActions<Branch> Branches { get; set; }
         public IDatabaseActions<BranchFacilities> BranchFacilities { get; set; }
-        public IDatabaseActions<User> Users { get; set; }
-        public IDatabaseActions<UserRoles> UserRoles { get; set; }
+        public IDatabaseActions<Court> Courts { get; set; }
         public IDatabaseActions<Facility> Facilities { get; set; }
         public IDatabaseActions<TimeSlot> TimeSlots { get; set; }
         public IDatabaseActions<Review> Reviews { get; set; }
-        public IDatabaseActions<Booking> Bookings { get; set; }
         public IDatabaseActions<Role> Roles { get; set; }
+        public IDatabaseActions<User> Users { get; set; }
+        public IDatabaseActions<UserRoles> UserRoles { get; set; }
+
     }
 }
